979e6e1d20113951b1e4f1bdf7e8bd4cc3e42e41
Gerard Tisseau <gerard.tisseau@free.fr>
2023-01-12 14:16:21 +0100

    Initial Commit

---
+++ LaboSudoku_FW/LaboSudoku_FW.h
@@ -0,0 +1,18 @@
+//
+//  LaboSudoku_FW.h
+//  LaboSudoku_FW
+//
+//  Created by Gérard Tisseau on 12/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LaboSudoku_FW.
+FOUNDATION_EXPORT double LaboSudoku_FWVersionNumber;
+
+//! Project version string for LaboSudoku_FW.
+FOUNDATION_EXPORT const unsigned char LaboSudoku_FWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaboSudoku_FW/PublicHeader.h>
+
+
